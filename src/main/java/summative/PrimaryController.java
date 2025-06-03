eba91da08d72cdd6d533b4f803bfc62849843b27
unknown <342861705@TDSB.ON.CA>
2025-06-03 11:17:51 -0400

    qayum blush !!!

--- src/main/java/summative/PrimaryController.java
+++ src/main/java/summative/PrimaryController.java
@@ -556,10 +556,10 @@ public class PrimaryController {
 
         gc.drawImage(originalImage, 0, 0, width, height);
 
-        double blushWidth = blushImage.getWidth() * 0.2;
-        double blushHeight = blushImage.getHeight() * 0.5;
-        double cx = width * 0.5 - blushWidth * 0.5;
-        double cy = height * 0.5 - blushHeight * 0.5;
+        double blushWidth = blushImage.getWidth() * 0.05;
+        double blushHeight = blushImage.getHeight() * 0.05;
+        double cx = width * 0.65 - blushWidth * 0.65;
+        double cy = height * 0.32 - blushHeight * 0.32;
 
         gc.drawImage(blushImage, cx, cy, blushWidth, blushHeight);
 
